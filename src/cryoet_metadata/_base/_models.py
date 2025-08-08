36b96decf9464ac0fbf16eacc62ebb4e25d91db1
Matt Iadanza <matthew.iadanza@stfc.ac.uk>
2025-08-08 22:15:22 +0100

    str to int

--- src/cryoet_metadata/_base/_models.py
+++ src/cryoet_metadata/_base/_models.py
@@ -334,7 +334,7 @@ class MovieFrame(AcquisitionMetadataMixin, Image2D):
     """
 
     path: Optional[str] = Field(default=None, description="""Path to a file.""")
-    section: Optional[str] = Field(default=None, description="""0-based section index to the entity inside a stack.""")
+    section: Optional[int] = Field(default=None, description="""0-based section index to the entity inside a stack.""")
     nominal_tilt_angle: Optional[float] = Field(
         default=None,
         description="""The tilt angle reported by the microscope""",
@@ -371,7 +371,7 @@ class ProjectionImage(AcquisitionMetadataMixin, Image2D):
     """
 
     path: Optional[str] = Field(default=None, description="""Path to a file.""")
-    section: Optional[str] = Field(default=None, description="""0-based section index to the entity inside a stack.""")
+    section: Optional[int] = Field(default=None, description="""0-based section index to the entity inside a stack.""")
     nominal_tilt_angle: Optional[float] = Field(
         default=None,
         description="""The tilt angle reported by the microscope""",
@@ -417,7 +417,7 @@ class SubProjectionImage(ProjectionImage):
 
     particle_index: Optional[int] = Field(default=None, description="""Index of a particle inside a tomogram.""")
     path: Optional[str] = Field(default=None, description="""Path to a file.""")
-    section: Optional[str] = Field(default=None, description="""0-based section index to the entity inside a stack.""")
+    section: Optional[int] = Field(default=None, description="""0-based section index to the entity inside a stack.""")
     nominal_tilt_angle: Optional[float] = Field(
         default=None,
         description="""The tilt angle reported by the microscope""",
