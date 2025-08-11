e0369ca9e831b27332f1095d4be1f8ea3392a45e
Matt Iadanza <matthew.iadanza@stfc.ac.uk>
2025-08-11 11:55:45 +0100

    Change to snake_case

--- src/cryoet_metadata/_base/_models.py
+++ src/cryoet_metadata/_base/_models.py
@@ -772,8 +772,8 @@ class MovieStackCollection(ConfiguredBaseModel):
         default=None,
         description="""The movie stacks in the collection""",
     )
-    DetectorGainFile: Optional[GainFile] = Field(default=None, description="""The gain file for the movie stacks""")
-    DetectorDefectFile: Optional[DefectFile] = Field(default=None, description="""The defect file for the movie stacks""")
+    gain_file: Optional[GainFile] = Field(default=None, description="""The gain file for the movie stacks""")
+    defect_file: Optional[DefectFile] = Field(default=None, description="""The defect file for the movie stacks""")
 
 
 class Dataset(ConfiguredBaseModel):
